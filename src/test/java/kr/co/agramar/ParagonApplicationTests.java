1012a3845801f1c23fc2474066838b25d75296fb
agramar <iamsjh1017@gmail.com>
2019-12-10 18:03:05 +0900

    Initial Commit

---
+++ src/test/java/kr/co/agramar/ParagonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.agramar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParagonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
